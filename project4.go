382737b693fb53beac32fb0ad102a449ee9bfbef
Lapa210 <IgorSmolyaninov@mail.ru>
2025-07-29 16:06:22 +0800

    priject4

--- project4.go
+++ project4.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	URL      string
+}
+
 func main() {
-	fmt.Println("Hello World")
+	login := promptData("Введите логин:")
+	password := promptData("Введите пароль:")
+	url := promptData("Введите URL:")
+
+	myAccount := account{login: login,
+		password: password,
+		URL:      url,
+	}
+
+	outputPassword(myAccount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt)
+	var res string
+	fmt.Scan(&res)
+	return res
+}
+
+func outputPassword(acc account) {
+	fmt.Println(acc.login, acc.password, acc.URL)
 }
